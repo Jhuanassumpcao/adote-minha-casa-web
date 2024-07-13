7fcf1be344a16880eeb8f71ed84935ced6f8160e
Jhuanassumpcao <jhuanassumpcao@gmail.com>
2024-07-13 16:55:51 -0300

    feat: reading the receipt value

--- src/views/DonationView.vue
+++ src/views/DonationView.vue
@@ -2,57 +2,89 @@
   <v-app>
     <Header />
     <v-container class="py-5">
-      <v-card class="pa-4">
-        <v-card-title class="headline mb-4">{{ title }}</v-card-title>
+      <v-card
+        :disabled="loading"
+        :loading="loading"
+        class="mx-auto"
+        max-width="374"
+      >
+        <v-img height="250" :src="image" cover></v-img>
+
+        <v-card-item>
+          <v-card-title>{{ title }}</v-card-title>
+
+          <v-card-subtitle>
+            <span class="me-1">
+              <v-icon left>mdi-city-variant-outline</v-icon> {{ city }} -
+              {{ state }}
+            </span>
+
+            <v-icon color="error" icon="mdi-fire-circle" size="small"></v-icon>
+          </v-card-subtitle>
+        </v-card-item>
+
         <v-card-text>
-          <v-row>
-            <v-col cols="12" class="mb-4">
-              <v-subheader class="subheader"
-                ><v-icon left>mdi-text-box-outline</v-icon>Descrição</v-subheader
-              >
-              <hr />
-              <p class="description">{{ description }}</p>
-            </v-col>
-            <v-col cols="12" class="mb-4">
-              <v-subheader class="subheader"
-                ><v-icon left>mdi-account-box</v-icon>Contato</v-subheader
-              >
-              <hr />
-              <p class="contact"><v-icon left>mdi-cash</v-icon> <strong>Pix:</strong> {{ pix }}</p>
-              <p class="contact">
-                <v-icon left>mdi-phone</v-icon> <strong>Telefone:</strong> {{ phone }}
-              </p>
-            </v-col>
-            <v-col cols="12" class="mb-4">
-              <v-subheader class="subheader"
-                ><v-icon left>mdi-map-marker</v-icon>Endereço</v-subheader
-              >
-              <hr />
-              <p class="address">
-                <v-icon left>mdi-home-outline</v-icon> <strong>Rua:</strong> {{ address }}
-              </p>
-              <p class="address">
-                <v-icon left>mdi-city</v-icon> <strong>Bairro:</strong> {{ neighborhood }}
-              </p>
-              <p class="address">
-                <v-icon left>mdi-city-variant-outline</v-icon> <strong>Cidade:</strong> {{ city }}
-              </p>
-              <p class="address">
-                <v-icon left>mdi-map</v-icon> <strong>Estado:</strong> {{ state }}
-              </p>
-              <p class="address">
-                <v-icon left>mdi-mailbox-outline</v-icon> <strong>CEP:</strong> {{ cep }}
-              </p>
-            </v-col>
-            <v-col cols="12" class="mb-4">
-              <v-subheader class="subheader"
-                ><v-icon left>mdi-currency-usd</v-icon>Valor Total Estimado</v-subheader
-              >
-              <hr />
-              <p class="estimated-value">R$ {{ value }}</p>
-            </v-col>
-          </v-row>
+          <div class="mb-4 text-subtitle-1">
+            <v-icon left>mdi-home-outline</v-icon> {{ address }} -
+            {{ neighborhood }}, {{ cep }}
+          </div>
+          <div class="my-4 text-subtitle-1">
+            <v-icon left>mdi-phone</v-icon> Telefone: {{ phone }}
+          </div>
+
+          <div>{{ description }}</div>
         </v-card-text>
+
+        <v-divider class="mx-4 mb-1"></v-divider>
+
+        <v-card-title>
+          Valor Total Estimado: <p class="estimated-value">R$ {{ value }}</p>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn color="deep-purple-lighten-2" text="Doar" block border @click="dialog = true"></v-btn>
+          <v-dialog v-model="dialog" width="auto">
+            <v-card max-width="400">
+              <v-card-title>Doar</v-card-title>
+              <v-card-text>
+                <v-form ref="form">
+                  <v-file-input
+                    v-model="donationProof"
+                    label="Comprovante"
+                    prepend-icon="mdi-camera"
+                    accept="image/*"
+                    required
+                  ></v-file-input>
+                  <v-text-field
+                    v-model="donationValue"
+                    label="Valor da Doação"
+                    type="number"
+                    required
+                    :disabled="loading || !donationValueExtracted"
+                  ></v-text-field>
+                  <v-textarea
+                    v-model="donationDescription"
+                    label="Descrição"
+                    rows="2"
+                    required
+                  ></v-textarea>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn 
+                  color="blue darken-1" 
+                  text 
+                  @click="submitDonation" 
+                  :loading="loading"
+                  :disabled="loading || !donationValueExtracted"
+                >
+                  Enviar
+                </v-btn>
+                <v-btn text @click="dialog = false">Cancelar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-card-actions>
       </v-card>
     </v-container>
     <Footer />
@@ -63,6 +95,7 @@
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import api from '@/services/api.js'
+import { ref } from 'vue'
 
 export default {
   name: 'DonationView',
@@ -81,7 +114,14 @@ export default {
       value: '',
       neighborhood: '',
       cep: '',
-      phoneNumber: ''
+      phone: '',
+      image: '',
+      dialog: false,
+      donationValue: '',
+      donationDescription: '',
+      donationProof: null,
+      loading: false,
+      donationValueExtracted: false
     }
   },
   async beforeMount() {
@@ -97,16 +137,90 @@ export default {
       this.neighborhood = data.bairro
       this.cep = data.cep
       this.phone = data.number
+      this.image = data.file_url || 'https://www.shutterstock.com/image-vector/house-logo-template-design-vector-600nw-741515455.jpg'
     } catch (error) {
       console.error('Erro ao obter dados das casas', error)
+    }
+  },
+  methods: {
+    submitDonation() {
+      const file = this.donationProof;
+      if (!file) {
+        this.result = 'Por favor, carregue uma imagem.';
+        return;
+      }
+      this.loading = true;
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const base64Image = reader.result.split(',')[1];
+        this.extractText(base64Image);
+      };
+      reader.readAsDataURL(file);
+    },
+    extractText(base64Image) {
+      Tesseract.recognize(
+        `data:image/png;base64,${base64Image}`, 
+        'por', 
+        { logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+        const valor = text.match(/R\$ ?\d{1,3}(?:\.\d{3})*,\d{2}/g);
+        if (valor) {
+          this.donationValue = this.convertToNumber(valor[0]);
+          this.donationValueExtracted = true;
+        } else {
+          this.result = 'Valor não encontrado na imagem.';
+          this.donationValueExtracted = false;
+        }
+        this.loading = false;
+      }).catch(error => {
+        console.error('Erro ao processar a imagem:', error);
+        this.result = 'Erro ao processar a imagem.';
+        this.loading = false;
+        this.donationValueExtracted = false;
+      });
+    },
+    convertToNumber(value) {
+      return parseFloat(value.replace("R$", "").replace(/\./g, "").replace(",", ".").trim());
+    }
+  },
+  watch: {
+    donationProof(comprovante) {
+      this.submitDonation(comprovante);
     }
   }
 }
 </script>
 
 <style scoped>
-.subheader {
-  font-size: 1.5rem;
+.v-card {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+.headline {
   font-weight: bold;
+  color: #2c3e50;
+}
+.subheader {
+  font-size: 1.25rem;
+  color: #34495e;
+}
+.v-divider {
+  margin: 10px 0;
+}
+.description,
+.contact,
+.address,
+.estimated-value {
+  font-size: 1rem;
+  color: #7f8c8d;
+}
+.image {
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+}
+.donate-btn {
+  font-size: 1.2rem;
+  padding: 14px 28px;
 }
 </style>
